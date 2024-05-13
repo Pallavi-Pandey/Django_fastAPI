4d89a1f449844301e9f088359cf7d3030dd637d7
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-05-13 18:59:36 +0530

    L-14

--- fast_delivery/core/customer/views.py
+++ fast_delivery/core/customer/views.py
@@ -3,5 +3,5 @@ from django.contrib.auth.decorators import login_required
 
 @login_required()
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'customer/base.html')
 
