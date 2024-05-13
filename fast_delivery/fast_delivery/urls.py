5c0b8dd002260f725a841f3731e573dc44b2e18a
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-05-13 19:23:40 +0530

    L-15

--- fast_delivery/fast_delivery/urls.py
+++ fast_delivery/fast_delivery/urls.py
@@ -8,6 +8,7 @@ from core.courier import views as courier_views
 
 customer_urlpatterns = [
     path('', customer_views.home, name="home"),
+    path('profile/', customer_views.profile_page, name="profile"),
 ]
 
 courier_urlpatterns = [
