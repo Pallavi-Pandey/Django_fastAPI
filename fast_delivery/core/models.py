fb79b4b7441c6f1467f22a60320379e70b98e1d1
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-05-16 11:17:24 +0530

    L-31

--- fast_delivery/core/models.py
+++ fast_delivery/core/models.py
@@ -1,5 +1,8 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 # Create your models here.
 class Customer(models.Model):
@@ -12,3 +15,50 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.user.get_full_name()
+    
+class Category(models.Model):
+    slug=models.CharField(max_length=255,unique=True)
+    name=models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+class Job(models.Model):
+    SMALL_SIZE="small"
+    MEDIUM_SIZE="medium"
+    LARGE_SIZE="large"
+
+    SIZES=[
+        (SMALL_SIZE,"small"),
+        (MEDIUM_SIZE,"medium"),
+        (LARGE_SIZE,"large"),
+    ]
+
+    CREATING_STATUS="creating"
+    PROCESSING_STATUS="processing"
+    PICKING_STATUS="picking"
+    DELIVERING_STATUS="delivering"
+    COMPLETED_STATUS="completed"
+    CANCELED_STATUS="canceled"
+
+    STATUSES=[
+        (CREATING_STATUS,"Creating"),
+        (PROCESSING_STATUS,"Processing"),
+        (PICKING_STATUS,"Picking"),
+        (DELIVERING_STATUS,"Delivering"),
+        (COMPLETED_STATUS,"Completed"),
+        (CANCELED_STATUS,"Canceled"),
+    ]
+
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    name=models.CharField(max_length=255)
+    description=models.CharField(max_length=255)
+    category=models.ForeignKey(Category,on_delete=models.SET_NULL,null=True,blank=True)
+    size=models.CharField(max_length=20,choices=SIZES,default=MEDIUM_SIZE)
+    quantity=models.IntegerField(default=1)
+    photo=models.ImageField(upload_to='job/photos/')
+    status=models.CharField(max_length=20,choices=STATUSES,default=CREATING_STATUS)
+    created_at=models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
