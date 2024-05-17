cb919e51b8f8532d6569343576b3476160aac048
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-05-17 11:07:25 +0530

    L-51

--- fast_delivery/core/courier/views.py
+++ fast_delivery/core/courier/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
+from django.conf import settings
 
 @login_required(login_url='/sign-in/?next=/courier/')
 def home(request):
@@ -8,4 +9,6 @@ def home(request):
 
 @login_required(login_url='/sign-in/?next=/courier/')
 def available_jobs_page(request):
-    return render(request, 'courier/available_jobs.html')
+    return render(request, 'courier/available_jobs.html', {
+        "GOOGLE_API_KEY" : settings.GOOGLE_API_KEY
+    })
